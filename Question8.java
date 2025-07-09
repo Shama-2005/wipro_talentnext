0e5e56084debf729488bc760c5cf33420d495b2c
Shama-2005 <shamaenugubala2005@gmail.com>
2025-07-09 19:17:38 +0530

    Add files via upload

--- Question8.java
+++ Question8.java
@@ -4,15 +4,9 @@ public class Question8 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		char a=sc.nextLine().charAt(0);
-		char b=sc.nextLine().charAt(0);
-		if(a>b) {
-			System.out.println(b+","+a);
-		}
-		else {
-			System.out.println(a+","+b);
-		}
-		
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		System.out.println((s1+s2).toLowerCase());
 	}
 
 }
